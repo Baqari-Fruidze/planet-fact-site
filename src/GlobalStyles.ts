ccda86280e1c026a2f26aef5d8c6cd3c342f15a0
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-05-27 23:34:05 +0400

    erore

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+export const Global = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html{
+    font-size: 62.5%;
+}
+body{
+    font-family: "League Spartan", sans-serif;
+}
+
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Global } from "./GlobalStyles.ts";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Global />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
